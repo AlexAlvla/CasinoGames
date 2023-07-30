b781eefafb4e4dcbc773553ddd6987df03ae17b5
Alexander <alexander.vlascenko@gmail.com>
2023-07-30 02:54:18 +0200

    Little refactoring for more structure

--- src/src/Games/BlackJack.java
+++ src/src/Games/BlackJack.java
@@ -1,3 +1,5 @@
+package Games;
+
 public class BlackJack {
     public BlackJack(){
         String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
--- src/src/Games/ThreeCardPoker.java
+++ src/src/Games/ThreeCardPoker.java
@@ -1,3 +1,5 @@
+package Games;
+
 public class ThreeCardPoker {
     public ThreeCardPoker(){
         String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
